814100514bf31194b21f22f702a082a85445dfd9
frocha <frocha@student.42heilbronn.de>
2024-11-06 16:14:14 +0100

    pipex updated

--- pipex.c
+++ pipex.c
@@ -10,14 +10,40 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <fcntl.h>
+#include <unistd.h>
+#include "libft/libft.h"
+#include <stdio.h>
+
+void check_existance_files(char *str, char *str2)
+{
+    int first_file_fd;
+    int second_file_fd;
+
+    first_file_fd = open(str, O_RDONLY);
+    if (first_file_fd == -1)
+    {
+        ft_putstr_fd(str,1);
+        ft_putstr_fd(": open: No such file or directory\n",1);
+    }
+    close(first_file_fd);
+    second_file_fd = open(str2, O_RDONLY);
+    if (second_file_fd == -1)
+    {
+        ft_putstr_fd(str2,1);
+        ft_putstr_fd(": open: No such file or directory\n",1);
+    }
+    close(second_file_fd);
+}
+
 int main(int argc, char **argv)
 {
     if (argc == 5)
     {
-
+        check_existance_files(argv[1], argv[4]);
     }else
     {
-        
+        ft_putstr_fd("Unexpected amount of arguments were given",1);
     }
     return (0);
 }
