00c59e48235d493fab77a044de0fd0a0874c7573
Franco Rocha <franco.rocha@fast2work.de>
2024-11-08 17:08:20 +0100

    norminette styling

--- pipex.c
+++ pipex.c
@@ -12,49 +12,66 @@
 
 #include "pipex.h"
 
-void handle_pipe_creation(int *fd)
+void	handle_pipe_creation(int *fd)
 {
-    if (pipe(fd) == -1)
-    {
-        perror("Error creating the pipe");
-        exit(EXIT_FAILURE);
-    }
+	if (pipe(fd) == -1)
+	{
+		perror("Error creating the pipe");
+		exit(EXIT_FAILURE);
+	}
 }
 
-void handle_number_of_arguments(int argc)
+void	handle_number_of_arguments(int argc)
 {
-    if (argc != 5)
-    {
-        perror("Unexpected amount of arguments were given\n");
-        exit(EXIT_FAILURE);
-    }
+	if (argc != 5)
+	{
+		ft_putstr_fd("Error: Incorrect number of arguments\n", 2);
+		exit(EXIT_FAILURE);
+	}
 }
 
-int main(int argc, char **argv, char **envp)
+void	wait_children(pid_t pid1, pid_t pid2)
 {
-    int fd[2];
-    int fd_infile;
-    int fd_outfile;
-    pid_t pid1, pid2;
+	waitpid(pid1, NULL, 0);
+	waitpid(pid2, NULL, 0);
+}
 
-    handle_number_of_arguments(argc);
-    handle_infile_opening(argv[1], &fd_infile);
-    handle_outfile_opening(argv[4], &fd_outfile);
-    handle_pipe_creation(fd);
-    if ((pid1 = fork()) == 0)
-    {
-        first_child(fd_infile, fd);
-        close_fd(fd_infile, fd_outfile, fd);
-        execute_command_and_fail(argv[2], envp);
-    }
-    if ((pid2 = fork()) == 0)
-    {
-        second_child(fd_outfile, fd);
-        close_fd(fd_infile, fd_outfile, fd);
-        execute_command_and_fail(argv[3], envp);
-    }
-    close_fd(fd_infile, fd_outfile, fd);
-    waitpid(pid1, NULL, 0);
-    waitpid(pid2, NULL, 0);
-    return (0);
+pid_t	create_child(pid_t *pid)
+{
+	*pid = fork();
+	if (*pid == -1)
+	{
+		perror("Error creating child process");
+		exit(EXIT_FAILURE);
+	}
+	return (*pid);
+}
+
+int	main(int argc, char **argv, char **envp)
+{
+	int		fd[2];
+	int		fd_infile;
+	int		fd_outfile;
+	pid_t	pid1;
+	pid_t	pid2;
+
+	handle_number_of_arguments(argc);
+	handle_infile_opening(argv[1], &fd_infile);
+	handle_outfile_opening(argv[4], &fd_outfile);
+	handle_pipe_creation(fd);
+	if (create_child(&pid1) == 0)
+	{
+		first_child(fd_infile, fd);
+		close_fd(fd_infile, fd_outfile, fd);
+		execute_command_and_fail(argv[2], envp);
+	}
+	if (create_child(&pid2) == 0)
+	{
+		second_child(fd_outfile, fd);
+		close_fd(fd_infile, fd_outfile, fd);
+		execute_command_and_fail(argv[3], envp);
+	}
+	close_fd(fd_infile, fd_outfile, fd);
+	wait_children(pid1, pid2);
+	return (0);
 }
