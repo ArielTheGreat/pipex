e999a928ed9d3e88ad4677a20ec4962950dfb3bc
frocha <frocha@student.42heilbronn.de>
2024-11-09 19:37:14 +0100

    norminette added to more_utils.c

--- more_utils.c
+++ more_utils.c
@@ -14,39 +14,39 @@
 
 void	first_child(char *infile_path, int *fd)
 {
-    int fd_infile;
+	int	fd_infile;
 
-    handle_infile_opening(infile_path, &fd_infile);
+	handle_infile_opening(infile_path, &fd_infile);
 	if (dup2(fd_infile, STDIN_FILENO) == -1)
-    {
-        perror("Error duplicating input file descriptor in first child");
-        exit(EXIT_FAILURE);
-    }
+	{
+		perror("Error duplicating input file descriptor in first child");
+		exit(EXIT_FAILURE);
+	}
 	if (dup2(fd[1], STDOUT_FILENO) == -1)
-    {
-        perror("Error duplicating pipe write end in first child");
-        exit(EXIT_FAILURE);
-    }
-    close(fd_infile);
+	{
+		perror("Error duplicating pipe write end in first child");
+		exit(EXIT_FAILURE);
+	}
+	close(fd_infile);
 	close(fd[0]);
 	close(fd[1]);
 }
 
-void	second_child(char   *outfile_path, int *fd)
+void	second_child(char *outfile_path, int *fd)
 {
-    int fd_outfile;
+	int	fd_outfile;
 
-    handle_outfile_opening(outfile_path, &fd_outfile);
+	handle_outfile_opening(outfile_path, &fd_outfile);
 	if (dup2(fd[0], STDIN_FILENO) == -1)
-    {
-        perror("Error duplicating pipe read in second child");
-        exit(EXIT_FAILURE);
-    }
+	{
+		perror("Error duplicating pipe read in second child");
+		exit(EXIT_FAILURE);
+	}
 	if (dup2(fd_outfile, STDOUT_FILENO) == -1)
-    {
-        perror("Error duplicating output file in second child");
-        exit(EXIT_FAILURE);
-    }
+	{
+		perror("Error duplicating output file in second child");
+		exit(EXIT_FAILURE);
+	}
 	close(fd_outfile);
 	close(fd[0]);
 	close(fd[1]);
